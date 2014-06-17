d55febe1f37baf907cc0de48d09fdf01500155c6
leekelleher <leekelleher@gmail.com>
2014-06-17 16:34:48 +0100

    Build bumped version number

--- src/Archetype.Courier/Properties/VersionInfo.cs
+++ src/Archetype.Courier/Properties/VersionInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.83.43876")]
+[assembly: AssemblyFileVersion("0.1.124.45224")]
 [assembly: AssemblyInformationalVersion("0.1.0")]
 
 
